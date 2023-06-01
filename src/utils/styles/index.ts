bc682f7cd3ec0205dda415c19705b262082185e6
Vinicius <ewerton.viniciusturco@gmail.com>
2023-05-31 22:55:13 -0300

    feat: add static colors

---
+++ src/utils/styles/index.ts
@@ -0,0 +1,11 @@
+export const COLORS = {
+  background: '#DDDDDD',
+  backgroundSecondary: '#FFFFFF',
+  borderColor: '#777777',
+  primary: '#7695EC',
+  secondary: '#CCCCCC',
+  save: '#47B960',
+  delete: '#FF5151',
+  text: '#000000',
+  textSecondary: '#FFFFFF',
+};
